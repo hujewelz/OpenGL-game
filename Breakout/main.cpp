124eb911f408f10149e214c1a9ba1aca9c202ec1
Jewelz <hujewelz@163.com>
2020-05-17 11:25:38 +0800

    Modify Demo

--- Breakout/main.cpp
+++ Breakout/main.cpp
@@ -1,13 +1,19 @@
 #include <window.hpp>
 #include <scene.hpp>
+#include <sprite.hpp>
+#include <vec.hpp>
 
 #include <iostream>
 
 int main(int argc, char *arge[])
 {
-    Scene s = Scene();
+
+    Sprite sprite("./Resources/demo.jpg", bet::vec2<float>(200.0f, 200.0f), bet::vec2<float>(100.0f, 100.0f));
+    Scene scene = Scene();
+    scene.AddChild(sprite);
+
     Window window(500, 600, "Breakout");
-    window.RunScene(s);
+    window.RunScene(scene);
 
     return 1;
 }
